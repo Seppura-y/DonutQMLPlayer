38deddce4eaac520a278a63a698811d404d6b0c9
Hiko <y7513414@Gmail.com>
2024-07-11 09:21:49 +0800

    更新到qt6.6.3

---
+++ build.bat
@@ -0,0 +1,6 @@
+set QT=G:\application/Qt/Qt6
+set QTSDK=%QT%\6.6.3\msvc2019_64
+set CMAKE=%QT%\Tools\CMake_64\bin\cmake
+set OUTPATH=.\bin
+@echo cmake to Vs2022 
+%CMAKE% -S . -B build -DCMAKE_PREFIX_PATH=%QTSDK%
